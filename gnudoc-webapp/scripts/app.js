b5cf3abd289f6f1b04d56cccc75d4444702eeac9
Nic Ferrier <nferrier@ferrier.me.uk>
2014-08-02 20:47:13 +0100

    remove a confusing comment

--- gnudoc-webapp/scripts/app.js
+++ gnudoc-webapp/scripts/app.js
@@ -118,9 +118,6 @@ $("#tocHidden").load(
         return { "href": a.attr("href"), "text": a.text() };
       }
     ).get();
-    // TODO: could add the parsed hrefs to a DOM node or to a global
-    // object or some object we decorate as we go through the call
-    // chain
     $("#contents").html(
       $(hrefs).map(function (i, e) {
         if (e.text != "") {
