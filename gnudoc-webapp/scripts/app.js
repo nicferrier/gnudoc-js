0c6dad2bbe6b9b46253db59034d0d1643287d52d
Nic Ferrier <nferrier@ferrier.me.uk>
2014-08-04 23:21:48 +0100

    reconstruct key bindings to support auto-complete

--- gnudoc-webapp/scripts/app.js
+++ gnudoc-webapp/scripts/app.js
@@ -1,25 +1,96 @@
 var util = require("util");
-var $ = require("jquery");
-var clickHistory = [];
+var $ = require("jquery"); window.$ = $; // useful debugging
+//var ac = require("jquery-ui/autocomplete");
+var Ac = require("triecomplete");
+
+var loadedIndex = {}; // Stores the GNU manual index
+var keymapMode = "normal"; // could be "normal" or "index"
+
+// Return a function that calls fn with the evt it's passed
+// But also evt.preventDefault on that evt
+function keyPreventDefault (fn) {
+  return function (evt) {
+    fn(evt);
+    evt.preventDefault();
+  };
+}
+
+function makeAutoComplete (evt) {
+  var ac = evt.target["ac"];
+  if (ac == null) {
+    console.log("making a new ac!");
+    ac = new Ac();
+    evt.target["ac"] = ac;
+    ac.initialize(Object.keys(loadedIndex));
+  }
+  return ac.search(evt.target.value).map(function (e) { return e.key; });
+}
 
 var commands = {
-  top: function () {
+  top: keyPreventDefault(function () {
     document.location.href = "/";
-  },
-  back: function () {
+  }),
+  back: keyPreventDefault(function () {
     window.history.go(-1);
-  },
-  forward: function () {
+  }),
+  forward: keyPreventDefault(function () {
     window.history.go(1);
+  }),
+  index: keyPreventDefault(function () {
+    keymapMode = "index";
+    $("#indexUi").toggleClass("hidden");
+    $("#indexTerm").focus();
+  }),
+  indexBackspace: function () {
+  },
+  indexComplete: function (evt) {
+    var comps = makeAutoComplete(evt);
+    if (comps[comps.length - 1].indexOf(comps[0]) == 0) {
+      $("#indexTerm").val(comps[0]);
+    }
+    evt.preventDefault();
+  },
+  indexAc: function (evt) {
+    console.log("indexAc called", evt.which);
+    var comps = makeAutoComplete(evt);
+    if (comps.length == 1) {
+      $("#indexTerm").val(comps[0]);
+      evt.preventDefault();
+      return;
+    }
+    console.log(util.format("choose %j", comps));
+  },
+  indexEscape: keyPreventDefault(function (evt) {
+    keymapMode = "normal";
+    $("#indexUi").addClass("hidden");
+  }),
+  indexLookup: function (evt) {
+    var term = $("#indexTerm").val();
+    commands.indexEscape(evt);
+    var target = loadedIndex[term];
+    history.pushState({}, "", target);
+    docGet("/info/" + target);
   }
 };
 
 var keymap = {
-  "<": commands.top,
-  "T": commands.top,
-  "l": commands.back,
-  "f": commands.forward
+  "normal": {
+    "<": commands.top,
+    "T": commands.top,
+    "L": commands.back,
+    "F": commands.forward,
+    "I": commands.index
+  },
+  "index": {
+    "DEFAULT": commands.indexAc
+  }
 };
+// Some of the keymap commands have to be specified as puts
+keymap["index"][String.fromCharCode(27)] = commands.indexEscape;
+keymap["index"][String.fromCharCode(13)] = commands.indexLookup;
+keymap["index"][String.fromCharCode(8)] = commands.indexBackspace;
+keymap["index"][String.fromCharCode(9)] = commands.indexComplete;
+
 
 $(document).ready(function (){
   if (document.location.pathname.indexOf("/info/") == 0) {
@@ -44,7 +115,6 @@ function docGet (infoTarget) {
   if (target == "/") {
     $("#viewer").addClass("hidden");
     $("#contents").removeClass("hidden");
-    clickHistory.push(target);
   }
   else {
     console.log("docGet: target = ", target);
@@ -64,7 +134,6 @@ function docGet (infoTarget) {
         $("#contents").addClass("hidden");
         $("#viewer").empty().html(body);
         $("#viewer").removeClass("hidden");
-        clickHistory.push(target);
         aReplace("#viewer a");
       }
     });
@@ -98,10 +167,15 @@ function aReplace (selector) {
 
 function keyDispatch (evt) {
   var keyStr = String.fromCharCode(evt.which);
-  var command = keymap[keyStr];
+  var command = keymap[keymapMode][keyStr];
   if (typeof(command) == "function") {
-    command();
-    evt.preventDefault();
+    command(evt);
+  }
+  else {
+    command = keymap[keymapMode]["DEFAULT"];
+    if (typeof(command) == "function") {
+      command(evt);
+    }
   }
 }
 
@@ -132,7 +206,28 @@ $("#tocHidden").load(
     aReplace("ul#contents li a");
 
     // Some basic "info" mode key handling
-    $("body").keypress(function (evt){ keyDispatch(evt); });
+    $("body").keydown(function (evt){ keyDispatch(evt); });
   });
+
+// Load the index - in dev right now
+$("#indexHidden").load(
+  "/manual/elisp/Index.html .index-fn",
+  function () {
+    $("#indexTerm").removeAttr("disabled");
+    $("#indexTerm").focus(function (evt) { 
+      // Reset the value so that the cursor is at the end
+      var value = evt.target.value;
+      evt.target.value = "";
+      evt.target.value = value;
+    });
+    $("#indexHidden li").each(function (i, e) {
+      var firstA = $($("a", e)[0]);
+      var key = firstA.text();
+      var ref = firstA.attr("href");
+      loadedIndex[key] = ref;
+    });
+    console.log("index is loaded");
+  }
+);
 
 // app.js ends here
